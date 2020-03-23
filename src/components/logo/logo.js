1dc8cf991ff17329eef8711d8210c2d7d10dce0b
Brandon Tripp <brandongtripp@gmail.com>
2020-03-22 19:18:37 -0600

    update of repository in root

---
+++ src/components/logo/logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './icons8-brain-64.png';
+import './logo.css';
+
+const Logo = () =>  {
+    return (
+        <div className='mas4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa4">
+                    <img style={{paddingTop: '6px'}}src={brain} alt="logo"/> 
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
